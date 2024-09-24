84c63d6e7206284da95c14ff3eaa73e1466f001e
fransfilastap@live.com <fransfilastap@Live.com>
2024-09-24 22:38:02 +0700

    Update API Key generation and ESLint configuration
    Refactor generateApiKeyAction to remove unused context parameter. Modify ESLint configuration to disable specific TypeScript and React Hooks rules.

--- web/src/app/(admin)/api-key/action.ts
+++ web/src/app/(admin)/api-key/action.ts
@@ -3,7 +3,7 @@
 import { actionClient } from "@/lib/safe-action";
 import { kontakClient } from "@/lib/kontak";
 
-const generateApiKeyAction = actionClient.action(async ({ ctx }) => {
+const generateApiKeyAction = actionClient.action(async () => {
   const apiKey = await kontakClient.generateAPIKey();
   return apiKey.api_key;
 });
