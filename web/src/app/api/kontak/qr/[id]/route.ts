b8f61325654572139234979b1aacaf47d7310d19
fransfilastap@live.com <fransfilastap@Live.com>
2024-09-24 22:04:19 +0700

    Refactor API endpoint to use NextRequest
    Updated the API route to utilize NextRequest instead of NextApiRequest for consistency with other routes. This change simplifies the import statements and aligns with the current project's best practices.

--- web/src/app/api/kontak/qr/[id]/route.ts
+++ web/src/app/api/kontak/qr/[id]/route.ts
@@ -1,9 +1,8 @@
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 import { kontakClient } from "@/lib/kontak";
 
 export async function GET(
-  req: NextApiRequest,
+  req: NextRequest,
 
   { params: { id } }: { params: { id: string } }
 ) {
